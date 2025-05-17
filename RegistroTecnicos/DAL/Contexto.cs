1c2265d3eefc2bd49510a65577611c0700b17828
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-17 16:09:59 -0400

    creando contexto

---
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore;
+using RegistroTecnicos.Models;
+
+namespace RegistroTecnicos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; } = null!;
+    }
+}
+
