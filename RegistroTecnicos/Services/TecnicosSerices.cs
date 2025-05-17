ae7cd895da2bf7bd4fe94de357c58e106e73767c
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-17 17:43:48 -0400

    creando metodo Guardar

---
+++ RegistroTecnicos/Services/TecnicosSerices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+
+namespace RegistroTecnicos.Services
+{
+    public class TecnicosSerices(IDbContextFactory<Contexto> DbFctory)
+    {
+        public async Task<bool> Guardar(Tecnicos tecnicos)
+        {
+            tecnicos.TecnicosId = tecnicos.TecnicosId;
+            if (!await Existe(tecnicos.TecnicosId))
+            {
+                return await Insertar(tecnicos);
+            }
+            else
+            {
+                return await Modificar(tecnicos);
+            }
+        }
+
+        
+    }
+}
