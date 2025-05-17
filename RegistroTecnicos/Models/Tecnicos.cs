f3fe5415790eb7367cc2f92e78bb0aaff53e21af
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-17 15:58:45 -0400

    creando clase de Tecnicos en carpeta Models

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicosId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string Conceptos { get; set; } = null!;
+}
