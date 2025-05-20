5e231f138ed2cf2e24d2d7303aa2587640a60fb1
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-19 21:02:31 -0400

    Corrige tenico  A tecnico

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
 
-namespace RegistroTecnicos.DAL
+namespace RegistroTecnicos.DAL;
+
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
-    {
-        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
-        public DbSet<Tecnicos> Tecnicos { get; set; }
-    }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
 }
-
