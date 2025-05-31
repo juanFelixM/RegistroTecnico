75e871030415475a91be98e2473a9f693124a6f7
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-31 16:31:54 -0400

    crea Contexto Tickets

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -9,4 +9,5 @@ public class Contexto : DbContext
 
     public DbSet<Tecnicos> Tecnicos { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Tickets> Tickets { get; set; }
 }
