421db6683fb0db26009645ba8c2ad1313dd73058
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-31 19:21:48 -0400

    agrega el ; al RegistroTecnicos.Models

--- RegistroTecnicos/Services/TicketsServices.cs
+++ RegistroTecnicos/Services/TicketsServices.cs
@@ -1,6 +1,67 @@
-﻿namespace RegistroTecnicos.Services
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services
 {
-    public class TicketsServices
+    public class TicketsServices(IDbContextFactory<Contexto> DbFactory)
     {
+        private async Task<bool> Existe(int ticketId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tickets
+                .AnyAsync(t => t.TicketId == ticketId);
+        }
+
+        private async Task<bool> Insertar(Tickets tickets)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Tickets.Add(tickets);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+
+        private async Task<bool> Modificar(Tickets tickets)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Update(tickets);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Guardar(Tickets tickets)
+        {
+            if (!await Existe(tickets.TicketId))
+            {
+                return await Insertar(tickets);
+            }
+            else
+            {
+                return await Modificar(tickets);
+            }
+        }
+
+        public async Task<Tickets?> Buscar(int ticketId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tickets
+                .FirstOrDefaultAsync(t => t.TicketId == ticketId);
+        }
+        public async Task<bool> Eliminar(int ticketId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tickets
+                .AsNoTracking()
+                .Where(t => t.TicketId == ticketId)
+                .ExecuteDeleteAsync() > 0;
+        }
+
+        public async Task<List<Tickets>> Listar(Expression<Func<Tickets, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tickets
+                .Where(criterio)
+                .AsNoTracking()
+                .ToListAsync();
+        }
     }
 }
