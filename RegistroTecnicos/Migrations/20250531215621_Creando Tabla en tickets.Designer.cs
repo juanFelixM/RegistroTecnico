1b9d4046f22f6765a55d5b37f7359b3fb106e8d4
juanFelixM <juan_maria1@ucne.edu.do>
2025-05-31 19:19:06 -0400

    actualiza la migracion

--- RegistroTecnicos/Migrations/20250531215621_Creando Tabla en tickets.Designer.cs
+++ RegistroTecnicos/Migrations/20250531215621_Creando Tabla en tickets.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RegistroTecnicos.DAL;
@@ -11,9 +12,11 @@ using RegistroTecnicos.DAL;
 namespace RegistroTecnicos.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250531215621_Creando Tabla en tickets")]
+    partial class CreandoTablaentickets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
