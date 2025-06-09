b863d6a03fb943ec4c262e39a54788398307e623
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-09 14:46:29 -0400

    agrega ; al final del NameSpace RegistroTecnicos

--- RegistroTecnicos/Models/Clientes.cs
+++ RegistroTecnicos/Models/Clientes.cs
@@ -1,29 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RegistroTecnicos.Models
+namespace RegistroTecnicos.Models;
+
+public class Clientes
 {
-    public class Clientes
-    {
-        [Key]
-        public int ClienteId { get; set; }
+    [Key]
+    public int ClienteId { get; set; }
 
-        [Required(ErrorMessage = "El Nombre es requerido")]
-        public string Nombres { get; set; } = string.Empty;
+    [Required(ErrorMessage = "El Nombre es requerido")]
+    public string Nombres { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "La Fecha de Ingreso es requerida")]
-        [DataType(DataType.Date)]
-        public DateTime FechaIngreso { get; set; }
+    [Required(ErrorMessage = "La Fecha de Ingreso es requerida")]
+    [DataType(DataType.Date)]
+    public DateTime FechaIngreso { get; set; }
 
-        [Required(ErrorMessage = "La Direccion es requerida")]
-        public string Direccion { get; set; } = string.Empty;
+    [Required(ErrorMessage = "La Direccion es requerida")]
+    public string Direccion { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El RNC es requerido")]
-        public string RNC { get; set; } = string.Empty;
+    [Required(ErrorMessage = "El RNC es requerido")]
+    public string RNC { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El Limite de Credito es requerido")]
-        public double LimiteCredito { get; set; }
+    [Required(ErrorMessage = "El Limite de Credito es requerido")]
+    public double LimiteCredito { get; set; }
 
-        [Required(ErrorMessage = "El Tecnico Encargado es requerido")]
-        public int TecnicoEncargadoId { get; set; }
-    }
+    [Required(ErrorMessage = "El Tecnico Encargado es requerido")]
+    public int TecnicoEncargadoId { get; set; }
 }
