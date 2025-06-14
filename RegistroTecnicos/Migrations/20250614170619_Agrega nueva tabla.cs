a2ce324d3e5ee1bbc6b4f1a9bf01d9aed4b3570f
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-14 13:08:38 -0400

    agrega tabla de sistemas en la base de datos

---
+++ RegistroTecnicos/Migrations/20250614170619_Agrega nueva tabla.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agreganuevatabla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sistemas",
+                columns: table => new
+                {
+                    SistemaId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Descripcion = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    Complejidad = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sistemas", x => x.SistemaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sistemas");
+        }
+    }
+}
