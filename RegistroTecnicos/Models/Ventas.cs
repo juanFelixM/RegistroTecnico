d0df32b9f75626dfcd13972e08f7fd20b848b5c2
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-19 09:37:13 -0400

    crea clase ventas

---
+++ RegistroTecnicos/Models/Ventas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Ventas
+{
+    [Key]
+    public int VentaId { get; set; }
+    public DateTime Fecha { get; set; }
+    public string ClienteId { get; set; } = "";
+    public double Monto { get; set; }
+
+}
