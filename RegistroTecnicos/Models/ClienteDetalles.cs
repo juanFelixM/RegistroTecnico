25bf8317cf1c410e236d414b24a0536a609b7037
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-19 08:58:58 -0400

    completa los campos de cliente detalles

--- RegistroTecnicos/Models/ClienteDetalles.cs
+++ RegistroTecnicos/Models/ClienteDetalles.cs
@@ -1,6 +1,16 @@
-﻿namespace RegistroTecnicos.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace RegistroTecnicos.Models;
+
+public class ClienteDetalles
 {
-    public class ClienteDetalles
-    {
-    }
+    [Key]
+    public int DetalleId { get; set; }
+    
+    public int ClienteId { get; set; }
+    
+    public int TipoId { get; set; }
+
+    public string Telefono { get; set; } = "";
 }
