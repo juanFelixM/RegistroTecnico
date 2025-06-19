2eae8f8edbfc78c9e85c7ca102633152a390c6de
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-19 09:17:16 -0400

    agrega OnModelCreating en el contexto

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -14,4 +14,16 @@ public class Contexto : DbContext
     public DbSet<ClienteDetalles> ClienteDetalles { get; set; }
     public DbSet<TiposTelefonos> TiposTelefonos { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TiposTelefonos>().HasData(new List<TiposTelefonos>()
+        {
+            new TiposTelefonos { TipoId = 1, Descripcion = "Telefono" },
+            new TiposTelefonos { TipoId = 2, Descripcion = "Celular" },
+            new TiposTelefonos { TipoId = 3, Descripcion = "Oficina" }
+        }); 
+        
+    }
+
 }
