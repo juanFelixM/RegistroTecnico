e4ab1c1b38fccec03c3152c521d8e707ae6b237d
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-19 09:37:03 -0400

    Crea case ventasDetalles

---
+++ RegistroTecnicos/Models/VentasDetalles.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistroTecnicos.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class VentasDetalles
+{
+    [Key]
+    public int Id { get; set; }
+    public int VentaId { get; set; }
+    public int SistemaId { get; set; }
+    public int Cantidad { get; set; }
+    public double Precio { get; set; }
+}
