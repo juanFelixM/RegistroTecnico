f8861a7495c518f67607ca857782fc2aed3b3e00
juanFelixM <juan_maria1@ucne.edu.do>
2025-06-26 19:39:29 -0400

    agrega parametros requeridos

--- RegistroTecnicos/Models/Ventas.cs
+++ RegistroTecnicos/Models/Ventas.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RegistroTecnicos.Models;
 
@@ -6,8 +7,21 @@ public class Ventas
 {
     [Key]
     public int VentaId { get; set; }
-    public DateTime Fecha { get; set; }
-    public string ClienteId { get; set; } = "";
-    public double Monto { get; set; }
 
+    [Required(ErrorMessage = "La Fecha es requerida")]
+    [DataType(DataType.Date)]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "El Cliente es requerido")]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "El Monto es requerido")]
+    public decimal Monto { get; set; }
+
+    [InverseProperty("Venta")]
+    public virtual ICollection<VentasDetalles> ventasDetalles { get; set; } = new List<VentasDetalles>();
+
+    [ForeignKey("ClienteId")]
+    [InverseProperty("Ventas")]
+    public virtual Clientes Cliente { get; set; } = null!;
 }
